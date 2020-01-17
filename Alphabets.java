25a5b0e44299956c87caf9f9795cd098171cb35f
root <root@ubuntu-jenkins-login.us-central1-a.c.cogent-tide-260407.internal>
2020-01-17 14:56:04 +0000

    Alphabet programm file added

---
+++ Alphabets.java
@@ -0,0 +1,10 @@
+class Alphabets
+{
+  public static void main(String args[])
+  {
+    char t;
+ 
+    for (t = 'A'; t <= 'Z'; t++) // For upper case use 'A' and 'Z'
+      System.out.println(t);
+   }
+}
